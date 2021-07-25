458dbd4339d59ed167b9627ecfcde1157c35c5db
Arteandr <artedamatik@gmail.com>
2021-07-25 23:20:56 +0300

    x

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -1,32 +1,32 @@
 import { HearManager } from "@vk-io/hear";
-import {  MessageContext, VK } from "vk-io";
+import { MessageContext, VK } from "vk-io";
 import CMD from "../cmd";
 import KickControllerCMD from "../commandControllers/kickController";
 import iCommand from "../interfaces/iCommand";
 
-export default class KickCommand implements iCommand{
+export default class KickCommand implements iCommand {
     name = "кик";
     description = "удалить пользователя из беседы";
     manager;
     cmd;
     vk;
 
-    constructor(manager: HearManager<MessageContext>,cmd: CMD, vk: VK){
+    constructor(manager: HearManager<MessageContext>, cmd: CMD, vk: VK) {
         this.manager = manager;
         this.cmd = cmd;
         this.vk = vk;
     }
-    
-    handler(): iCommand{
-        this.manager.hear(/^кик(?:\s+(?<text>.+)|$)/i,async (ctx) => { 
-            if(ctx.chatId){
-                if(ctx.hasReplyMessage ){
-                    await KickControllerCMD.kick(ctx.replyMessage!.senderId,ctx.chatId,this.vk);
-                }else if(!ctx.hasReplyMessage){
-                    if(ctx.$match[1]){
-                    const userId = Number(ctx.$match[1].replace(/\D+/g,""));
-                    await KickControllerCMD.kick(userId,ctx.chatId,this.vk);
-                    }else {
+
+    handler(): iCommand {
+        this.manager.hear(/^кик(?:\s+(?<text>.+)|$)/i, async (ctx) => {
+            if (ctx.chatId) {
+                if (ctx.hasReplyMessage) {
+                    await KickControllerCMD.kick(ctx.replyMessage!.senderId, ctx.chatId, this.vk);
+                } else if (!ctx.hasReplyMessage) {
+                    if (ctx.$match[1]) {
+                        const userId = Number(ctx.$match[1].replace(/\D+/g, ""));
+                        await KickControllerCMD.kick(userId, ctx.chatId, this.vk);
+                    } else {
                         await ctx.send("Используйте: кик @user");
                     }
                 }
